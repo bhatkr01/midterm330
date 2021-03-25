83edd05b2b9457098b930a76e9c7cf373d342313
Kritib Bhattarai <bhatkr01@kritibs-mbp.lc.luther.edu>
2021-03-25 01:12:46 -0500

    c5

--- js/controller.js
+++ js/controller.js
@@ -2,30 +2,66 @@
 /* jshint browser: true */
 /* jshint node: true */
 'use strict';
-const competitions_code=["PL","CL"]
-function populateSelect(selectId, competition_list) {
-    let sel = document.getElementById(selectId);
-    for (let item of competition_list){
-        let anOption=document.createElement("option");
-        anOption.setAttribute("value", item);
-        anOption.innerHTML=item;
-        sel.appendChild(anOption);
-    }
-}
-// var full_data={}
+
 
 async function get_football_data(selected_id) {
-
-    let competition_name=document.querySelector("#competition").value;
-    var full_data= await fetch(`https://api.football-data.org/v2/competitions/${competition_name}/matches`,{
+    var full_data= await fetch(`https://api.football-data.org/v2/competitions/PL/matches`,{
         headers : { 'X-Auth-Token': '2e028e92d6ed4a75b8e3ceee3a045715' }
     })
     .then(response => response.json());
+     selected_id.innerHTML="";
+    for (let index=0; index<full_data.matches.length;index++){
+        localStorage.setItem("local_storage", JSON.stringify(full_data));
+        display_matches(full_data.matches[index],selected_id);
+    }
+}
 
+// async function display_matches(data_set, passed_class){
+async function display_matches(...options){
+    let row=document.createElement("div");
+    let away_div=document.createElement("button");
+    let home_div=document.createElement("button");
+    let time_div=document.createElement("div");
+    let score_div=document.createElement("div");
+    let match_details_div=document.createElement("button");
+    let hr_item=document.createElement("hr");
+    hr_item.classList.add("hr_line");
+    row.classList.add("main_box");
+    away_div.classList.add("away_team");
+    away_div.addEventListener("click", () => display_team_info(options[0].awayTeam.name));
+    home_div.classList.add("home_team");
+    home_div.addEventListener("click", () => display_team_info(options[0].homeTeam.name));
+    time_div.classList.add("time");
+    score_div.classList.add("score");
+    match_details_div.classList.add("match_details");
+    time_div.innerHTML=options[0].utcDate;
+    away_div.innerHTML=options[0].awayTeam.name;
+    home_div.innerHTML=options[0].homeTeam.name;
+    if (options[0].score.fullTime.awayTeam!=null || options[0].score.fullTime.homeTeam!=null){
+        score_div.innerHTML=options[0].score.fullTime.awayTeam+"-"+options[0].score.fullTime.homeTeam;
+       
+    }
+    else{
+        score_div.innerHTML="To Be played";  
+    }
+    if (options[0].homeTeam.name!=null || options[0].awayTeam.name!=null ){
+        match_details_div.innerHTML="Match Details";
+        match_details_div.addEventListener("click", () => display_match_details(options[0].homeTeam.name,options[0].awayTeam.name));
+    }
+    else{
+        match_details_div.innerHTML="To Be Played";
+    }
+    row.appendChild(time_div);
+    row.appendChild(away_div);
+    row.appendChild(score_div);
+    row.appendChild(home_div);
+    row.appendChild(match_details_div);
+    options[1].appendChild(row);
+    options[1].appendChild(hr_item);
+    options[1].style.border="5px solid purple";
 
-    var match_details= await fetch(`https://apiv2.apifootball.com/?action=get_events&from=2020-08-01&to=${new Date().toJSON().slice(0,10)}&league_id=148&APIkey=fab4e5052b71bd0d93e476b678086fc46d690efb1d50b1486a66dbfa91ab3e3d`)
-    .then(response => response.json());
-
+}
+async function display_team_info(team_name){
     var team_id= await fetch("https://elenasport-io1.p.rapidapi.com/v2/seasons/3260/teams?page=1", {
         "method": "GET",
         "headers": {
@@ -33,178 +69,87 @@ async function get_football_data(selected_id) {
             "x-rapidapi-host": "elenasport-io1.p.rapidapi.com"
         }
     }).then(response => response.json());
-    console.log(team_id)
-    
-    // for (let index2=0; index2<team_id.length;index2++){
-    //             if (full_data.matches[index1].awayTeam.name.includes(match_details[index2].match_awayteam_name) && full_data.matches[index1].homeTeam.name.includes(match_details[index2].match_hometeam_name) ){
-    //                 console.log(match_details[index2].statistics);
-    //                 console.log(match_details[index2].match_hometeam_name);
-    //                 console.log(match_details[index2].match_awayteam_name);
-    //             }
-    //         }
-            
-    
-
-
-    // let video_data= await fetch(`https://www.scorebat.com/video-api/v1`)
-    // .then(response => response.json());
-    // let new_video_data=[];
-    // for (let index=0; index<video_data.length;index++){
-    //     if (video_data[index].competition.name==="ENGLAND: Premier League"){
-    //         new_video_data.push(video_data[index]);
-    //     }
-        
-    // }
-    // console.log(new_video_data);
-
-     selected_id.innerHTML="";
-
-    if (document.querySelector("#competition").value==="CL"){
-        let main_stage_box=document.createElement("div");
-        let group_stage=document.createElement("div");
-        let round_of_16=document.createElement("div");
-        let quarter_finals=document.createElement("div");
-        let semi_finals=document.createElement("div");
-        let final=document.createElement("div");
-        main_stage_box.classList.add("main_stage_box");
-        group_stage.classList.add("group_stage");
-        round_of_16.classList.add("round_of_16");
-        quarter_finals.classList.add("quarter_finals");
-        semi_finals.classList.add("semi_finals");
-        final.classList.add("final");
-        group_stage.innerHTML="Group Stage";
-        round_of_16.innerHTML="Round Of 16";
-        quarter_finals.innerHTML="Quarter Finals";
-        semi_finals.innerHTML="Semi Finals";
-        final.innerHTML="Finals";
-        main_stage_box.appendChild(group_stage);
-        main_stage_box.appendChild(round_of_16);
-        main_stage_box.appendChild(quarter_finals);
-        main_stage_box.appendChild(semi_finals);
-        main_stage_box.appendChild(final);
-        selected_id.appendChild(main_stage_box);
-
-
-        for (let index=0; index<full_data.matches.length;index++){
-                if (full_data.matches[index].stage==="GROUP_STAGE"){
-                    display_matches(full_data.matches[index], document.querySelector(".group_stage"));
-                }
-                else if (full_data.matches[index].stage==="ROUND_OF_16"){
-                    display_matches(full_data.matches[index],document.querySelector(".round_of_16"));
-                    }
-                else if (full_data.matches[index].stage==="QUARTER_FINALS"){
-                        display_matches(full_data.matches[index],document.querySelector(".quarter_finals"));
-                        }
-                else if (full_data.matches[index].stage==="SEMI_FINALS"){
-                        display_matches(full_data.matches[index],document.querySelector(".semi_finals"));
-                    }
-                else if (full_data.matches[index].stage==="FINAL"){
-                    display_matches(full_data.matches[index],document.querySelector(".final"));
-                }
+    for (let i=0; i<team_id.data.length;i++){
+        if (team_id.data[i].fullName==team_name){
+            var team_info= await fetch(`https://elenasport-io1.p.rapidapi.com/v2/teams/${team_id.data[i].id}`, {
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-key": "1521a9ed46msh1dbaeb2927d89fap129614jsn4a1066daf85c",
+                "x-rapidapi-host": "elenasport-io1.p.rapidapi.com"
             }
-
-    
+            }).then(response =>response.json());
+            }
     }
-    else if (document.querySelector("#competition").value==="PL"){
-        for (let index=0; index<full_data.matches.length;index++){
-            display_matches(full_data.matches[index],selected_id,match_details);
-            // display_matches(full_data.matches[index],selected_id);
+    document.querySelector("#match_info").innerHTML="";
+    let details_div=document.createElement("div");
+    details_div.innerHTML=`<img src=${team_info.data[0].badgeURL}>`
+    for (let info in team_info.data[0]){
+        if (info!="badgeURL" && info!="id"){    
+           details_div.innerHTML+=info+":"+ " "+ team_info.data[0][info]+ "\n";
+           details_div.style.whitespace = "pre-line";
+        }
+        
+    }
+    details_div.classList.add("details");
+    document.querySelector("#match_info").appendChild(details_div);
+    
 
+
+}
+
+async function display_match_details(hometeam,awayteam){
+    var matchdetails= await fetch(`https://apiv2.apifootball.com/?action=get_events&from=2020-08-01&to=${new Date().toJSON().slice(0,10)}&league_id=148&APIkey=fab4e5052b71bd0d93e476b678086fc46d690efb1d50b1486a66dbfa91ab3e3d`)
+    .then(response => response.json());
+    console.log(matchdetails)
+    for (let index2=0; index2<matchdetails.length;index2++){
+        let details_div=document.createElement("div");
+        details_div.classList.add("details");
+        if (awayteam.includes(matchdetails[index2].match_awayteam_name) && hometeam.includes(matchdetails[index2].match_hometeam_name) ){
+            document.querySelector("#match_info").innerHTML="";
+            let title=document.createElement("h1");
+            title.classList.add("tile_of_match");
+            let match_stats=document.createElement("div");
+            match_stats.classList.add("match_stats");
+            title.innerHTML=matchdetails[index2].match_awayteam_name+" "+"VS"+" "+matchdetails[index2].match_hometeam_name;
+            for (let i=0;i<matchdetails[index2].statistics.length; i++){
+                let row=document.createElement("div");
+                row.classList.add("main_box");
+                let away_div=document.createElement("div");
+                away_div.classList.add("away_stats");
+                let home_div=document.createElement("div");
+                home_div.classList.add("home_stats");
+                let category_div=document.createElement("div");
+                category_div.classList.add("home_stats");
+                away_div.innerHTML= matchdetails[index2].statistics[i].away;
+                category_div.innerHTML=matchdetails[index2].statistics[i].type;
+                home_div.innerHTML=matchdetails[index2].statistics[i].home;
+                row.appendChild(away_div);
+                row.appendChild(category_div);
+                row.appendChild(home_div);
+                match_stats.appendChild(row) 
+            }
+            details_div.appendChild(title);
+            details_div.appendChild(match_stats);
+            document.querySelector("#match_info").appendChild(details_div);
+           
         }
     }
-}
-
-// async function display_matches(data_set, passed_class){
-async function display_matches(...options){
-    let row=document.createElement("div");
-    let away_div=document.createElement("div");
-    let versus_div=document.createElement("div");
-    let home_div=document.createElement("div");
-    let time_div=document.createElement("div");
-    let score_div=document.createElement("div");
-    let highlight_div=document.createElement("div");
-    let match_details_div=document.createElement("div");
-    row.classList.add("main_box");
-    away_div.classList.add("away_team");
-    versus_div.classList.add("versus");
-    home_div.classList.add("home_team");
-    time_div.classList.add("time");
-    score_div.classList.add("score");
-    highlight_div.classList.add("highlight");
-    match_details_div.classList.add("match_details");
-    time_div.innerHTML=options[0].utcDate;
-    away_div.innerHTML=options[0].awayTeam.name;
-    versus_div.innerHTML="VS";
-    home_div.innerHTML=options[0].homeTeam.name;
-    score_div.innerHTML=options[0].score.fullTime.awayTeam+"-"+options[0].score.fullTime.homeTeam
-    match_details_div.innerHTML="Match Details";
-    match_details_div.addEventListener("click", () => display_match_details(options[0].homeTeam.name,options[0].awayTeam.name,options[2]));
-    // match_details_div.innerHTML="<a href='match_details.html' target='_blank'>Match Details</a>";
-    row.appendChild(time_div);
-    row.appendChild(away_div);
-    row.appendChild(versus_div);
-    row.appendChild(home_div);
-    row.appendChild(score_div);
-    row.appendChild(match_details_div);
-    options[1].appendChild(row);
 
 }
-async function display_match_details(hometeam,awayteam, matchdetails){
-        for (let index2=0; index2<matchdetails.length;index2++){
-            if (awayteam.includes(matchdetails[index2].match_awayteam_name) && hometeam.includes(matchdetails[index2].match_hometeam_name) ){
-                document.querySelector("#match_info").innerHTML="";
-                let title=document.createElement("h2");
-                title.classList.add("tile_of_match");
-                let details=document.createElement("div");
-                details.classList.add("details");
-                title.innerHTML=matchdetails[index2].match_awayteam_name+"VS"+matchdetails[index2].match_hometeam_name;
-                for (let i=0;i<matchdetails[index2].statistics.length; i++){
-                    details.innerHTML+=matchdetails[index2].statistics[i].type+ ":" + matchdetails[index2].statistics[i].away+ " " + matchdetails[index2].statistics[i].home + `\n`;
-                }
-                document.querySelector("#match_info").appendChild(title);
-                document.querySelector("#match_info").appendChild(details);
-                
-                console.log(matchdetails[index2].statistics);
-                // console.log(match_details[index2].match_hometeam_name);
-                // console.log(match_details[index2].match_awayteam_name);
-                
-              
-            }
-        }
-
-}
-
-        
-//         // 
-//         // row.classList.add("main_box");
-//         // div1.classList.add("number_itself");
-//         // div2.classList.add("smth_about_number");
-//         // div1.innerHTML=number.number;
-//         // div2.innerHTML=number.text;
-//         // row.appendChild(div1);
-//         // row.appendChild(div2);
-//         // all_numbers.appendChild(row);
-            
-//         }
-
-    
-    
-    
-
 
 
 async function clickedon() {
     get_football_data(document.querySelector("#match_info"));
-    // if (document.querySelector('#batch').checked) {
-    //     get_batch(num, all_numbers);
-    // } else {
-    //     get_individual(num, all_numbers);
-    // }
 }
 
 window.onload = function() {
-    
-    populateSelect("competition", competitions_code);
+    let match_list=localStorage.getItem("local_storage");
+    if (match_list){
+        match_list=JSON.parse(match_list);
+        for (let index=0; index<match_list.matches.length;index++){
+            display_matches(match_list.matches[index],document.querySelector("#match_info"));
+        }
+    }
     
 };
 
